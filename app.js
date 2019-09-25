190ed1b5868635f89e57249496cb5e36c227971e
VerryGustiAndrea <verrygustiandrea@gmail.com>
2019-09-25 13:13:40 +0700

    penambahan popup berhasil registrasi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const flash = require('connect-flash');
-const session = require('express-session')
+const session = require('express-session');
 
 
 
@@ -31,11 +31,11 @@ app.use(session({
   }));
 
 
-//Connecr flash
+//Connect flash
 app.use(flash());
 
 //Global Vars
-app.use((req, res) =>{
+app.use((req, res, next) =>{
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     next();
--- routes/users.js
+++ routes/users.js
@@ -71,7 +71,7 @@ router.post('/register', (req, res) =>{
                      //save user
                     newUser.save()
                         .then(user =>{
-                            req.flash('succes_msg', 'You are now registered and can log in');
+                            req.flash('success_msg', 'You are now registered and can log in');
                             res.redirect('/users/login');
                         })
                         .catch(err => console.log(err));
